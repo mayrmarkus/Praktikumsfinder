5905507c74b144cd5404f6166fafbea0ec08dfa6
mayrmarkus <43027477+mayrmarkus@users.noreply.github.com>
2021-05-25 13:14:05 +0200

    Add files via upload

---
+++ Frontend/praktikums-finder/build/precache-manifest.69e63d52a6443684dd8d05cb01d3b6cc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7de71c5f4029a74c3cadea312b5b68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ecb1794247a3a35a876",
+    "url": "/static/css/2.c90d4508.chunk.css"
+  },
+  {
+    "revision": "fc6c809c05ba994c0c0d",
+    "url": "/static/css/main.e320b2d0.chunk.css"
+  },
+  {
+    "revision": "6ecb1794247a3a35a876",
+    "url": "/static/js/2.eb513743.chunk.js"
+  },
+  {
+    "revision": "2cc235483de5d543a76f6dfe16648355",
+    "url": "/static/js/2.eb513743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc6c809c05ba994c0c0d",
+    "url": "/static/js/main.0bf36e2b.chunk.js"
+  },
+  {
+    "revision": "e0bade0b35e2db97badd",
+    "url": "/static/js/runtime-main.6edf7ea6.js"
+  },
+  {
+    "revision": "409951ebb0d41c24bec382844cf63874",
+    "url": "/static/media/LogoVertical.409951eb.png"
+  },
+  {
+    "revision": "73c01f8ba30fd74755013d9f08984ce1",
+    "url": "/static/media/contract.73c01f8b.jpg"
+  },
+  {
+    "revision": "5ffbbfa2ef5cf01d22f96f94cbc7200c",
+    "url": "/static/media/download.5ffbbfa2.png"
+  },
+  {
+    "revision": "5ffbbfa2ef5cf01d22f96f94cbc7200c",
+    "url": "/static/media/final.5ffbbfa2.png"
+  },
+  {
+    "revision": "bca039d82faf5c854cb85bff74f3aaee",
+    "url": "/static/media/login.bca039d8.jpeg"
+  }
+]);
